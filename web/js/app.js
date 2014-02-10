6ba48d2a2f3d0a8dc1d6978587be99dd840bf37a
Maximo Martinez <git@maxm.uy>
2014-02-09 23:56:56 -0200

    working on visualization

--- web/js/app.js
+++ web/js/app.js
@@ -4,16 +4,35 @@
   var pollPulseMillis = 0
 
   var lastPulses = []
-  var chartsReady = false
 
   var pollPulseEnd = null
 
   var pollTimer = null
 
-  google.load('visualization', '1.0', {'packages':['corechart']});
-  google.setOnLoadCallback(function() { chartsReady = true; });
+  var canvas = null;
+  var context = null;
+
+  var baseLine = 50;
+
+  var hover = 0;
 
   $(document).ready(function() {
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+
+    $(canvas).mousemove(mousemove)
+    document.addEventListener('touchdown', function(e) {
+        e.preventDefault();
+        var touch = e.touches[0];
+        mousemove(touch);
+    }, false);
+
+    $( window ).resize(function() {
+      drawChart();
+    });
+
+    pollPulseEnd = new Date().getTime();
+    
     setPulseHourSpan(6);
     pollPulses();
     updateNow();
@@ -54,37 +73,85 @@
   }
 
   function drawChart() {
-    if(!chartsReady) return;
-    var watts = [['Time', 'Power']];
-    var lastT = 0;
-    var total = 0;
-    $.each(lastPulses, function(i,t) {
-      if (i > 0) {
-        ++total;
-        watts.push([new Date(t[0]), deltaToWatts(t[1])])
-      }
-      lastT = t;
+    canvas.width  = window.innerWidth - 50;
+    canvas.style.width = canvas.width + "px"
+    
+    var max = 0;
+    $.each(lastPulses, function(i, p) {
+      max = Math.max(max, deltaToWatts(p[1]));
     })
-    var data = google.visualization.arrayToDataTable(watts);
 
-    var options = {
-      fontSize: 14,
-      hAxis: {
-        format: 'MMM d, HH:mm'
-      },
-      vAxis: { 
-        viewWindow:{
-          min: 0
-        }
-      },
-      chartArea: {
-        height: 500
-      },
-      legend: {position: 'none'}
+    context.beginPath();
+    context.strokeStyle = "#0000aa";
+    context.lineWidth = 1;
+    var segments = canvas.width / 2;
+    for (var i = 0; i < segments; ++i) {
+      var x = i / segments * canvas.width;
+      var from = (pollPulseEnd - pollPulseMillis) + pollPulseMillis * i / segments;
+      var to = (pollPulseEnd - pollPulseMillis) + pollPulseMillis * (i + 1) / segments;
+      var y = canvas.height - averagePower(from, to)/max*(canvas.height - baseLine) - baseLine;
+      if (i == 0) {
+        context.moveTo(x,y); 
+      } else {
+        context.lineTo(x,y); 
+      }
     };
+    context.stroke();
 
-    var chart = new google.visualization.LineChart(document.getElementById('chart'));
-    chart.draw(data, options);
+    {
+      var from = (pollPulseEnd - pollPulseMillis) + pollPulseMillis * hover / canvas.width;
+      var power = averagePower(from, from + pollPulseMillis / segments);
+      context.beginPath();
+      context.fillStyle = "#0000aa";
+      var y = canvas.height - power/max*(canvas.height - baseLine) - baseLine;
+      context.arc(hover,y,4,0,2*Math.PI);
+      context.fill();
+
+      context.font="16px Helvetica";
+      var text = Math.round(power) + "W " + moment(from).format('MMM D, HH:mm:ss');;
+      var size = context.measureText(text);
+      var x = hover + 3;
+      y += 15;
+      if (x + size.width > canvas.width) x = canvas.width - size.width;
+      context.fillStyle = "black";
+      context.fillText(text, x, y);
+    }
+    
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(0, canvas.height - baseLine);
+    context.lineTo(canvas.width, canvas.height - baseLine);
+    context.stroke();
+  }
+
+  function mousemove(event) {
+    hover = event.pageX - $(canvas).offset().left;
+    drawChart();
+  }
+
+  function averagePower(from, to) {
+    var power = 0;
+    var powerCount = 0;
+    var closest = null;
+    var closestDist = 0;
+    $.each(lastPulses, function(i, p) {
+      var dist = Math.abs(p[0] - from);
+      if (closest == null || dist < closestDist) {
+        closest = p;
+        closestDist = dist;
+      }
+      if (p[0] >= from && p[0] <= to) {
+        power += deltaToWatts(p[1]);
+        powerCount++;
+      }
+    });
+    if (powerCount > 0) return power / powerCount;
+    
+    if (closest != null && closestDist < 60*60*1000/5) {
+      return deltaToWatts(closest[1]);
+    }
+    return 0;
   }
 
   function deltaToWatts(delta) {
